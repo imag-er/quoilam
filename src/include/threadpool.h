282746b8af4fce214988b0d4fa1e7a75c05c7bde
imag-er <2274265784@qq.com>
2023-03-02 23:27:29 +0800

    cmakefile optimized

--- src/include/threadpool.h
+++ src/include/threadpool.h
@@ -4,6 +4,7 @@
 #include <queue>
 #include <future>
 #include <mutex>
+#include <condition_variable>
 namespace quoilam
 {
     class ThreadPool
@@ -22,18 +23,21 @@ namespace quoilam
         std::queue<std::function<void()>> tasks;
         std::mutex lock;
         std::vector<std::thread> worker_threads;
+        std::condition_variable cv;
     };
     template<class F, class... Args>
     auto ThreadPool::push_task(F&& f, Args&&... args) -> std::future<decltype(f(args...))>
     {
         using return_t = decltype(f(args...));
         using task_t = decltype(std::packaged_task<return_t>);
-        
+
         auto task_ptr = std::make_shared<task_t>(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
 
-        
+    
+
+
     }
 
 }
