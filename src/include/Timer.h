1df8a970d0f4bbb3e97439e95ce7f377a96d55da
imag-er <2274265784@qq.com>
2023-03-12 13:34:34 +0800

    sync timer support

--- src/include/Timer.h
+++ src/include/Timer.h
@@ -16,11 +16,11 @@ namespace quoilam
             Argu&&... argus
         ) -> decltype(f(argus...));
 
-        template < class Func, class... Argu>
-        auto async(
-            Func&& f,
-            Argu&&... argus
-        ) -> std::future<decltype(f(argus...))>;
+        // template < class Func, class... Argu>
+        // auto async(
+        //     Func&& f,
+        //     Argu&&... argus
+        // ) -> std::future<decltype(f(argus...))>;
 
     private:
 
@@ -40,21 +40,21 @@ namespace quoilam
     }
 
 
-    template < class Func, class... Argu>
-    auto Timer::async(
-        Func&& f,
-        Argu&&... argus
-    ) -> std::future<decltype(f(argus...))>
-    {
-        // 这里打包了f(args...)并且把它作为函数返回值
-        using task_type = std::packaged_task<decltype(f(argus...))(void)>;
+    // template < class Func, class... Argu>
+    // auto Timer::async(
+    //     Func&& f,
+    //     Argu&&... argus
+    // ) -> std::future<decltype(f(argus...))>
+    // {
+    //     // 这里打包了f(args...)并且把它作为函数返回值
+    //     using task_type = std::packaged_task<decltype(f(argus...))(void)>;
 
-        auto async_task = std::make_shared<task_type>(
-            std::bind(std::forward<Func>(f), std::forward<Argu>(argus)...)
-        );
+    //     auto async_task = std::make_shared<task_type>(
+    //         std::bind(std::forward<Func>(f), std::forward<Argu>(argus)...)
+    //     );
 
-        return async_task->get_future();
-    }
+    //     return async_task->get_future();
+    // }
 
 };
 
